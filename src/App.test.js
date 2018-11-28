49907592349a8525d8d1c539ce25b6ce530fe41a
Allen Joseph <allen.joseph.v.a@gmail.com>
2018-11-27 20:00:36 -0500

    test(app): update test setup and app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-testing-library';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('App container must has a header, section and footer', () => {
+  const { container } = render(<App />);
+  expect(container.getElementsByTagName('header').length).toBe(1);
+  expect(container.getElementsByTagName('section').length).toBe(1);
+  expect(container.getElementsByTagName('footer').length).toBe(1);
 });
